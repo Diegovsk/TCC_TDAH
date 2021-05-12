cf56496b2e57220e05dabacec15dbe8ac1ec5682
Diego Moraes <diegorafaelb.demoraes@gmail.com>
2021-05-12 17:51:48 -0300

    implementando a tela das referencias, mas tem que corrigir

--- app/src/main/java/br/com/alexandre/tdah/MenuActivity.java
+++ app/src/main/java/br/com/alexandre/tdah/MenuActivity.java
@@ -48,6 +48,14 @@ public class MenuActivity extends AppCompatActivity {
         mp.start();
     }
 
+    public void info(View view) {
+        Intent intent = new Intent(this, MenuInfoActivity.class);
+        startActivity(intent);
+
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.click);
+        mp.start();
+    }
+
     @Override
     public void finish() {
         super.finish();
---
+++ app/src/main/java/br/com/alexandre/tdah/MenuInfoActivity.java
@@ -0,0 +1,31 @@
+package br.com.alexandre.tdah;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuInfoActivity extends AppCompatActivity {
+    public MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void next(View view){
+        Intent intent  = new Intent(this, MenuInfoActivity.class);
+        ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeCustomAnimation(getApplicationContext(),R.transition.fade_in, R.transition.mover_direita);
+        ActivityCompat.startActivity(MenuInfoActivity.this, intent, activityOptionsCompat.toBundle());
+
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.click);
+        mp.start();
+    }
+}
